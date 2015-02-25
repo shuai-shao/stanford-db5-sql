33fca5634f820a75f27a7adaf7cf328bec7bd073
Shuai <sshaoking@gmail.com>
2015-02-25 00:18:37 -0800

    Update social-network-query-exercises.sql

--- social-network-query-exercises.sql
+++ social-network-query-exercises.sql
@@ -1,16 +1,16 @@
-Students at your hometown high school have decided to organize their social network using databases. So far, they have collected information about sixteen students in four grades, 9-12. Here's the schema: 
+#Students at your hometown high school have decided to organize their social network using databases. So far, they have collected information about sixteen students in four grades, 9-12. Here's the schema: 
 
-Highschooler ( ID, name, grade ) 
-English: There is a high school student with unique ID and a given first name in a certain grade. 
+#Highschooler ( ID, name, grade ) 
+#English: There is a high school student with unique ID and a given first name in a certain grade. 
 
-Friend ( ID1, ID2 ) 
-English: The student with ID1 is friends with the student with ID2. Friendship is mutual, so if (123, 456) is in the Friend table, so is (456, 123). 
+#Friend ( ID1, ID2 ) 
+#English: The student with ID1 is friends with the student with ID2. Friendship is mutual, so if (123, 456) is in the Friend table, so is (456, 123). 
 
-Likes ( ID1, ID2 ) 
-English: The student with ID1 likes the student with ID2. Liking someone is not necessarily mutual, so if (123, 456) is in the Likes table, there is no guarantee that (456, 123) is also present. 
+#Likes ( ID1, ID2 ) 
+#English: The student with ID1 likes the student with ID2. Liking someone is not necessarily mutual, so if (123, 456) is in the Likes table, there is no guarantee that (456, 123) is also present. 
 
-Q1  (1/1 point)
-Find the names of all students who are friends with someone named Gabriel. 
+#Q1  (1/1 point)
+#Find the names of all students who are friends with someone named Gabriel. 
 SELECT name 
 FROM   Highschooler
        JOIN Friend
@@ -22,8 +22,8 @@ WHERE  ID1
         WHERE  name = 'Gabriel'
        )
 
-Q2  (1/1 point)
-For every student who likes someone 2 or more grades younger than themselves, return that student's name and grade, and the name and grade of the student they like. 
+#Q2  (1/1 point)
+#For every student who likes someone 2 or more grades younger than themselves, return that student's name and grade, and the name and grade of the student they like. 
 SELECT h1.name, h1.grade, h2.name, h2.grade
 FROM   Likes
        JOIN Highschooler h1
@@ -32,8 +32,8 @@ FROM   Likes
          ON Likes.ID2 = h2.ID
 WHERE  h1.grade - h2.grade >=2
 
-Q3  (1/1 point)
-For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order. 
+#Q3  (1/1 point)
+#For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order. 
 SELECT DISTINCT h1.name, h1.grade, h2.name, h2.grade
 FROM   Likes l1 
        JOIN Highschooler h1
@@ -47,8 +47,8 @@ FROM   Likes l1
          OR l2.ID2 = h2.ID
 WHERE  h1.name < h2.name
 
-Q4  (1/1 point)
-Find all students who do not appear in the Likes table (as a student who likes or is liked) and return their names and grades. Sort by grade, then by name within each grade. 
+#Q4  (1/1 point)
+#Find all students who do not appear in the Likes table (as a student who likes or is liked) and return their names and grades. Sort by grade, then by name within each grade. 
 SELECT name,
        grade
 FROM   Highschooler 
@@ -59,8 +59,8 @@ UNION
 SELECT ID2 
 FROM   Likes)
 
-Q5  (1/1 point)
-For every situation where student A likes student B, but we have no information about whom B likes (that is, B does not appear as an ID1 in the Likes table), return A and B's names and grades. 
+#Q5  (1/1 point)
+#For every situation where student A likes student B, but we have no information about whom B likes (that is, B does not appear as an ID1 in the Likes table), return A and B's names and grades. 
 SELECT a.name, 
        a.grade,
        b.name,
@@ -74,8 +74,8 @@ WHERE l.ID2 NOT IN
       (SELECT ID1
        FROM   Likes)
        
-Q6  (1/1 point)
-Find names and grades of students who only have friends in the same grade. Return the result sorted by grade, then by name within each grade. 
+#Q6  (1/1 point)
+#Find names and grades of students who only have friends in the same grade. Return the result sorted by grade, then by name within each grade. 
 SELECT DISTINCT h.name,
        h.grade
 FROM   Highschooler h
@@ -92,8 +92,8 @@ WHERE  f.ID1 NOT IN
         )
 ORDER BY h.grade, h.name
 
-Q7  (1/1 point)
-For each student A who likes a student B where the two are not friends, find if they have a friend C in common (who can introduce them!). For all such trios, return the name and grade of A, B, and C. 
+#Q7  (1/1 point)
+#For each student A who likes a student B where the two are not friends, find if they have a friend C in common (who can introduce them!). For all such trios, return the name and grade of A, B, and C. 
 SELECT a.name,
        a.grade,
        b.name,
@@ -112,13 +112,13 @@ FROM   Highschooler a
 WHERE  f2.ID2 NOT IN (SELECT ID2 FROM Friend WHERE ID1 = f1.ID1)
 AND    f2.ID2 IN (SELECT ID2 FROM Likes WHERE ID1 = f1.ID1)
 
-Q8  (1/1 point)
-Find the difference between the number of students in the school and the number of different first names. 
+#Q8  (1/1 point)
+#Find the difference between the number of students in the school and the number of different first names. 
 SELECT COUNT(ID) - COUNT(DISTINCT name)
 FROM   Highschooler
 
-Q9  (1/1 point)
-Find the name and grade of all students who are liked by more than one other student. 
+#Q9  (1/1 point)
+#Find the name and grade of all students who are liked by more than one other student. 
 SELECT name, grade 
 FROM   Likes
        JOIN Highschooler 
